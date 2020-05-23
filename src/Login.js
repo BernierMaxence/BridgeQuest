fa0d15f4df296c6b0a85a05cb5881132facd19bf
pbernier <pacomebernier@orange.fr>
2020-05-23 12:16:36 +0200

    [pbernier] - V.0.1 5/6

--- src/Login.js
+++ src/Login.js
@@ -4,10 +4,10 @@ import { withNavigation } from 'react-navigation';
 import SafeAreaView from 'react-native-safe-area-view';
 import LinearGradient from 'react-native-linear-gradient';
 
-
 class Login extends React.Component{
     constructor(props){
         super(props);
+        this._onPressLogin = this._onPressLogin.bind(this)
         this.state = {
             login: '',
         }
@@ -16,37 +16,38 @@ class Login extends React.Component{
     _onChangeText(text) {
         this.setState({login: text});
     }
+
+    _onPressLogin() {
+      const {navigate} = this.props.navigation
+      const {login} = this.state
+      if (login) {
+        navigate("DisplayQR", {login})
+      }
+    }
+
     render(){
-        const {navigate} = this.props.navigation;
-
-        return(
-            <SafeAreaView style={styles.container}>
-            <LinearGradient
-                style={styles.container}
-                start={{x: 0.0, y: 0.0}} end={{x: 1.0, y: 1.0}}
-                locations={[0,1,1]}
-                colors={['#4c669f', '#3b5998', '#192f6a']}
-            >
-                    <Image
-                    style={{width: 200, height: 200}}
-                    source={require('./assets/logo.png')}
-                />
-                <TextInput
-                    style={{ width: 200, height: 30, borderColor:'#e5fdfe' , borderRadius: 4, borderWidth: 1, padding: 5, backgroundColor:'#e5fdfe' }}
-                    onChangeText={text => this._onChangeText(text)}
-                    maxLength={40}
-                />
-                <Button
-                    onPress={() => navigate("DisplayQR", {login: this.state.login})}
-                    title={'Go'}
-                />
-            </LinearGradient>
-
-            </SafeAreaView>
+      return(
+          <SafeAreaView style={styles.container}>
+          <LinearGradient
+            style={styles.container}
+            start={{x: 0.0, y: 0.0}} end={{x: 1.0, y: 1.0}}
+            locations={[0,1,1]}
+            colors={['#4c669f', '#3b5998', '#192f6a']}>
+              <Image
+                style={{width: 200, height: 200}}
+                source={require('./assets/logo.png')}/>
+              <TextInput
+                style={{ width: 200, height: 30, borderColor:'#e5fdfe' , borderRadius: 4, borderWidth: 1, padding: 5, backgroundColor:'#e5fdfe' }}
+                onChangeText={text => this._onChangeText(text)}
+                maxLength={40}/>
+              <Button
+                onPress={this._onPressLogin}
+                title={'Go'}/>
+          </LinearGradient>
+          </SafeAreaView>
         )
     }
 }
-
 
 const styles = StyleSheet.create({
     container: {
